a30325f009da09d81a0b2382d4dbdc3c0e32a56b
KDunin <krzysiekdunin@gmail.com>
2019-03-25 18:36:36 +0100

    Packages version bump, build fix

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -6,41 +6,42 @@ import Test from '../../views/Test/Test'
 import AdminPanel from '../../views/AdminPanel/AdminPanel'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
-const Routes = [{
+const Routes = [ {
   path:      '/',
   component: Homepage,
   exact:     true,
-},{
+}, {
   path:      '/training',
   component: Training,
   exact:     false,
-},{
+}, {
   path:      '/test',
   component: Test,
   exact:     false,
-},{
+}, {
   path:      '/admin',
   component: AdminPanel,
   exact:     false,
-}]
+} ]
 
 const timeout = { enter: 600, exit: 0 }
 /* eslint-disable react/prop-types */
 const Router = ({ location }) => {
-  const currentKey = location.pathname.split('/')[1] || '/'
+  const currentKey = location.pathname.split('/')[ 1 ] || '/'
   return (
     <TransitionGroup>
       <CSSTransition
-        key={currentKey}
+        key={ currentKey }
         classNames='fade'
-        timeout={timeout}
+        timeout={ timeout }
       >
-        <Switch location={location}>
-          {renderRoutes()}
+        <Switch location={ location }>
+          { renderRoutes() }
         </Switch>
       </CSSTransition>
     </TransitionGroup>
-  )}
+  )
+}
 
 export default withRouter(Router)
 
@@ -48,11 +49,11 @@ const renderRoutes = () => (
   Routes.map(({ path, component, exact }, index) => {
     return (
       <Route
-        key={index}
-        path={path}
-        component={component}
-        exact={exact}
-      /> 
+        key={ index }
+        path={ path }
+        component={ component }
+        exact={ exact }
+      />
     )
   })
 )
